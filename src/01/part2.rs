2f7ccd0546a2f38149214c57b3b9e5252dd6c0d7
Youva GAUDÉ <youv.didj@gmail.com>
2025-12-02 00:10:49 +0100

    Day 1 part 2 done

--- src/01/part2.rs
+++ src/01/part2.rs
@@ -1,4 +1,27 @@
-pub fn solve(s: &str) -> String {
-    let _ = s;
-    "".into()
+fn step_over_zero(mut n: i32) -> i32 {
+    if n <= 0 {
+        n -= 1;
+    }
+    n.div_euclid(100).abs()
+}
+
+pub fn solve(s: &str) -> i32 {
+    s.split_whitespace()
+        .map(|s| {
+            let (dir, n) = s.split_at(1);
+            match dir {
+                "R" => n.parse::<i32>().unwrap(),
+                "L" => -n.parse::<i32>().unwrap(),
+                _ => unreachable!(),
+            }
+        })
+        .fold((0, 50), |(mut count, start), n: i32| {
+            let delta = start + n;
+            count += step_over_zero(delta);
+            if start == 0 && delta < 0 {
+                count -= 1;
+            }
+            let end = delta.rem_euclid(100);
+            (count, end)
+        }).0
 }
